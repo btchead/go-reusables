c692f1dd3f8955b3cd9a6a5474446f4957f348e0
core <core@btchead.click>
2025-07-21 19:39:04 +0300

    Fixes yaml template generation
    - Adds missing yaml struct tags

--- log/config.go
+++ log/config.go
@@ -1,7 +1,7 @@
 package log
 
 type Config struct {
-	Level   string `json:"level" default:"info" validate:"required,oneof=debug warn info error"`
-	Format  string `json:"format" default:"json" validate:"required,oneof=json console"`
-	Colored bool   `json:"colored" default:"false"`
+	Level   string `json:"level" yaml:"level" default:"info" validate:"required,oneof=debug warn info error"`
+	Format  string `json:"format" yaml:"format" default:"json" validate:"required,oneof=json console"`
+	Colored bool   `json:"colored" yaml:"colored" default:"false"`
 }
